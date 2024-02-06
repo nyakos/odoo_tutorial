48c9a46ade2f0976f49c2fd9eadd82eea632919f
AkosNyiry <113112440+AkosNyiry@users.noreply.github.com>
2024-02-06 17:07:34 +0100

    adding estate_property_tag model

---
+++ estate/models/estate_property_tag.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class EstatePropertyTag(models.Model):
+    _name = 'estate.property.tag'
+    _description = 'Estate property tag'
+
+    name = fields.Char(
+        required=True
+    )
